cc3484375664ffe89efb7d56462d4bc3e30a9d64
lichen0319 <lichenbook@qq.com>
2017-08-07 23:28:34 -0700

    play color

--- htdocs/test.c
+++ htdocs/test.c
@@ -2,8 +2,10 @@
 int  main()                                                   
 {                                                                 
 char buf[1024] = {0};
-
+char color[16] = {0};
+/* color=xxxx */
 read(0, buf, sizeof(buf));
+sscanf(buf, "color=%s\n", color);
 
     printf("Content-type: text/html\n\n");                        
     printf("<html>\n");                                            
@@ -11,12 +13,14 @@ read(0, buf, sizeof(buf));
     printf("<title>CGI Output</title>\n");                         
     printf("</head>\n");                                           
 
-    printf("<body>");                                             
-    printf("<h1> Hellow, world. </h1>");                           
+    printf("<body bgcolor=\"%s\">", color);                                             
+    printf("<h1> Hello world! </h1>"); 
 
+#if 1
 printf("<h2>");
-printf("content: %s", buf);
+printf("%s", buf);
 printf("</h2>");
+#endif
 
     printf("</body>");                                           
     printf("</html>\n");                                         
